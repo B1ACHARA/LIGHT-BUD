41f480997f0167cf729405d97ffa5f12fa377950
KociakYT <jakubpracharczyk4@gmail.com>
2025-02-09 14:19:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.querySelector(".preloader").classList.add("preloader-hidden");
+    }, 3000);
+});
+
